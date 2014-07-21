4ff905c3c7e006c42c37259b99dea35d212dd453
zhang1984 <jzhang@studio-hamburg.de>
2014-07-21 18:19:09 +0200

    set the documentation with doxygen

--- config/cardtype.h
+++ config/cardtype.h
@@ -4,17 +4,45 @@
 #include <QString>
 #include <QList>
 
+/*!
+  \brief
+  define the card type
+*/
 class cardtype
 {
 public:
+    /**
+        \brief initial the class*/
     cardtype();
+    /**
+    \brief
+    initial the class
+    \param index : the index of the cardtype
+    \param Name : the name of the class
+    \param channelList : the list of channels, which under the card
+    */
     cardtype(int index, QString Name, QList< int> channelList);
+
     int index;
     QString name;
     QList< int> channelList;
 
+    /**
+    \brief
+    compare function, whether is the same card.
+    */
     int compare(cardtype _card);
+
+    /**
+    \brief
+    return the card name
+    */
     QString getName(QList<cardtype> cardList, int index);
+
+    /**
+    \brief
+    return the channel list under the card
+    */
     QList<int> getChannelList(QList<cardtype> cardList, QString name);
 };
 
--- config/filecontrol.h
+++ config/filecontrol.h
@@ -14,33 +14,120 @@
 #include <QDomDocument>
 #include <QTime>
 #include <tuple>
-
+/**
+    \brief
+    r/w the files in the server, get the information
+*/
 class filecontrol
 {
 public:
+    /**
+    \brief
+    initial
+    */
     filecontrol();
+    /**
+      \brief initial with the ip adress or host name
+    */
     filecontrol(QString ipAdd);
 
+    /**
+    \brief
+    \param ipAdd : the ip adress or host name
+    */
     QString ipAdd;
+    /**
+    \brief
+    \param configFile : the config.xml in the server with the path /usr/local/ubecontrol/config.xml
+    */
     QFile* configFile;
+    /**
+    \brief
+    \param statusFile : the status.xml in the server with the path /usr/local/ubecontrol/status.xml
+    */
     QFile* statusFile;
+    /**
+    \brief
+    \param logFile : the dvs_sdi.log in the server with the path /Storage/[project name]/dvs_sdi.log
+    */
     QFile* logFile;
+    /**
+    \brief
+    \param uberecorderFile : the uberecorder file in the server with the path /usr/local/etc/uberecorder.conf
+    */
     QFile* uberecorderFile;
+    /**
+    \brief
+    \param videoChannelFile : the video channel file in the server with the path /usr/local/etc/vc_new.conf save the information about the channel under each video card
+    */
     QFile* videoChannelFile;
+    /**
+        \brief
+        \param orderFile : the order File file in the server with the path /usr/local/ubecontrol/order.xml
+        */
     QFile* orderFile;
+    /**
+      \brief
+    \param projectName : the name of project
+    */
     QString projectName;
+    /**
+    \brief
+    \param serverName : the name of server
+    */
     QString serverName;
+    /**
+    \brief
+    \param serverID : the id number of server
+    */
     QString serverID;
+    /**
+    \brief logPos : the current read position of the dvs_sdi.log
+    */
     qint64 logPos;
 
-
+    /**
+    \brief initial the config file
+    */
     int iniConfigFile();
+
+    /**
+    \brief initial the video channel file
+    */
     int iniVideoChannelFile();
-    int setFile(QString ipAAd, int noDialog = 0);
+    /**
+    \brief initial the files path
+    \param ipAdd : ip address
+    \param noDialog : with/without the dialog-GUI
+    */
+    int setFile(QString ipAdd, int noDialog = 0);
+
+    /**
+    \brief test, whether all the necessary files are existed in the server
+    */
     QList< int> isServer();
+
+    /**
+    \brief read the config file, return the serverType
+    */
     serverType readConfigFile();
+
+    /**
+    \brief read the status file, return whether the capture is started (1) or stopped (0)
+    */
     int getStatus();
+
+    /**
+    \brief read the logFile, save a list of messageType, and change the logPos to the last position of log file
+    \param ipAdd : the ip address
+    \param messageList : the list of messageType
+    \param pos : the begin position of the reader
+    */
     QList<messageType> readLogFile(QString ipAdd, QList<messageType> messageList, int pos = 0);
+
+    /**
+    \brief
+    */
     QString readUberecorderFile();
     int removeFormat(QString ipAdd, QString channel, QString streamID, QString format);
     int removeStream(QString ipAdd, QString channel, QString streamID);
