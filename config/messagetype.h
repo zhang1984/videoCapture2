2cd86df2f1c72d7b88bf8b56ba868dfcf61e28ab
zhang1984 <jzhang@studio-hamburg.de>
2014-07-29 13:45:17 +0200

    update the messageType documentation

--- config/messagetype.h
+++ config/messagetype.h
@@ -6,21 +6,53 @@
 #include <QStringList>
 #include <QDebug>
 
+/**
+\brief define the message type from a line in dvs_sdi.log file
+*/
 class messageType
 {
     //Q_OBJECT
 public:
+    /**
+    \brief initial the messageType
+    */
     explicit messageType();
+    /**
+    \brief initial the messageType with the parameter of the messageType
+    */
     messageType(int datum, QString time, QString device, QString message, int type);
-
+    /**
+    \brief initial the messageType from the line text code in dvs_sdi.log
+    */
     messageType(QString line);
+    /**
+    \brief
+    \param datum : the day of the text
+    */
     int datum;
+    /**
+    \brief
+    \param time : the time code of the text
+    */
     QString time;
+    /**
+    \brief
+    \param message : the message text
+    */
     QString message;
+    /**
+    \brief
+    \param device : the device of the capture computer
+    */
     QString device;
-    ///////////good = 1; Error = 2; Warning = 3; Sync = 4;
+    /**
+    \brief
+    \param type : the type of the message. 1 = Good; 2 = Error; 3 = Warning; 4 = Sync Error
+    */
     int type;
-
+    /**
+    \brief  hand out the message type from the message
+    */
     int getMessageType(QString message);
 
 //signals:
