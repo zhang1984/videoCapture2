65137f4f6110a15fb7e5c34c97c84aa13bf8c2c1
zhang1984 <jzhang@studio-hamburg.de>
2014-07-29 14:02:57 +0200

    update the serverType documentation

--- config/servertype.h
+++ config/servertype.h
@@ -5,19 +5,58 @@
 #include <config/cardtype.h>
 #include <config/streamtype.h>
 #include <QList>
-
+/**
+\brief define the server type
+*/
 class serverType
 {
 public:
+    /**
+    \brief
+    initial the serverType
+    */
     serverType();
+    /**
+    \brief initial the serverType with parameters
+    */
     serverType(QString projectName, QString ipAdd, QString serverName);
+    /**
+    \brief initial the serverType with parameters and the information about the streams and video cards
+    */
     serverType(QString projectName, QString ipAdd, QString serverName, QList< streamtype> streamList, QList< cardtype> cardList);
+    /**
+    \brief
+    \param projectName : the name of project
+    */
     QString projectName;
+    /**
+    \brief
+    \param ipAdd : the IP Address of the server
+    */
     QString ipAdd;
+    /**
+    \brief
+    \param serverName :the name of the server
+    */
     QString serverName;
+    /**
+    \brief
+    \param streamList : the list of the streamType in the server
+    */
     QList< streamtype> streamList;
+    /**
+    \brief
+    \param cardList : the list of the cardType in the server
+    */
     QList< cardtype> cardList;
+    /**
+    \brief
+    \param IOCapture : status of the server capture, 0 = stopped, 1 = started
+    */
     int IOCapture;
+    /**
+    \brief get the video card name according to the number of channel
+    */
     QString getVideoCard(int channel);
 };
 
