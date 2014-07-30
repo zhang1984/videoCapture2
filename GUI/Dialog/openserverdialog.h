260aa1aea38d5138d61babb12ca87b42f063a960
zhang1984 <jzhang@studio-hamburg.de>
2014-07-30 10:35:03 +0200

    update servereditdialog documentation

--- GUI/Dialog/openserverdialog.h
+++ GUI/Dialog/openserverdialog.h
@@ -6,14 +6,36 @@
 #include <QPushButton>
 #include <QGridLayout>
 #include <QWidget>
-
+/**
+\brief
+the class defines a open server dialog, to input the server ip address
+*/
 class openServerDialog
 {
 public:
+    /**
+    \brief initial the open server dialog
+    */
     openServerDialog();
+    /**
+    \brief
+    \param openServer : a dialog interface
+    */
     QDialog* openServer;
+    /**
+    \brief
+    \param serverEdit : a line edit for input the server address
+    */
     QLineEdit* serverEdit;
+    /**
+    \brief
+    \param okButton : a button ojbect to accept the input
+    */
     QPushButton* okButton;
+    /**
+    \brief
+    \param cancelButton : a button ojbect to close the windows without input
+    */
     QPushButton* cancelButton;
 };
 
