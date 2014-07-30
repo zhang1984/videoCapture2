345da1f4aa3641012eba80db6445ef4a179ab959
zhang1984 <jzhang@studio-hamburg.de>
2014-07-30 16:17:25 +0200

    update centralwidget document

--- GUI/centralwidget.h
+++ GUI/centralwidget.h
@@ -20,18 +20,54 @@
 #include <config/filecontrol.h>
 #include <tuple>
 
+/**
+    define a widget shows items options
+*/
 class centralwidget : public QWidget
 {
     Q_OBJECT
 public:
+    /**
+    \brief initial the centralwidget
+    */
     explicit centralwidget(QWidget *parent = 0);
+    /**
+    \brief
+    \param the widget of the class
+    */
     QWidget* centralWidget;
+    /**
+    \brief
+    \param codecList : the list of all the resolutions
+    */
     QStringList codecList;
+    /**
+    \brief
+    \param currentTimeDisplay : the LCD display for current time
+    */
     QLCDNumber* currentTimeDisplay;
+    /**
+    \brief
+    \param durationDisplay : the LCD display for duration
+    */
     QLCDNumber* durationDisplay;
+    /**
+    \brief
+    \param lastSplitDisplay : the LCD display for last split
+    */
     QLCDNumber* lastSplitDisplay;
+    /**
+    \brief
+    \param nextSplitDisplay : the LCD display for next split
+    */
     QLCDNumber* nextSplitDisplay;
+
     QLabel* captureLabel;
+
+    /**
+    \brief
+    \param _timeWidget : the widget for the all timecode display layout
+    */
     QWidget* _timeWidget;
 
     QTableWidget* attrTable;
@@ -61,15 +97,28 @@ public:
 
     void createTimeWidget();
     void createTable();
-
+    /**
+    \brief set the server option in widget
+    */
     void setData(serverType _server);
+    /**
+      \brief set the server and stream option in widget
+    */
     void setData(serverType _server, streamtype _stream);
+    /**
+      \brief set the server, stream and format option in widget
+    */
     void setData(serverType _server, streamtype _stream, formatType _format);
-
+    /**
+    \brief test, if the status.xml is enabled, yes, return 1, else return 0
+    */
     int setFileControl(QString ipAdd);
 signals:
 
 public slots:
+    /**
+    \brief update the timecode in display from status.xml
+    */
     void updateTime();
 
 };
