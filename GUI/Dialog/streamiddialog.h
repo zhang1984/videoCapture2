2508d377d423211e2926f3bc44f8c8623c16ffd0
zhang1984 <jzhang@studio-hamburg.de>
2014-07-30 14:32:26 +0200

    update streaiddialog documentation

--- GUI/Dialog/streamiddialog.h
+++ GUI/Dialog/streamiddialog.h
@@ -8,20 +8,43 @@
 #include <QGridLayout>
 #include <QDialog>
 #include <QDebug>
+/**
+\brief the class of server id dialog, the parameter in config.xml as "id" in server tag
+*/
 class streamiddialog : public QWidget
 {
     Q_OBJECT
 public:
+    /**
+    \brief initial the class
+    */
     explicit streamiddialog(QWidget *parent = 0);
+    /**
+    \brief
+    \param dialog : window interface as dialog
+    */
     QDialog* dialog;
+    /**
+    \brief
+    \param okButton : ok button
+    */
     QPushButton* okButton;
-
+    /**
+    \brief
+    \param idEdit : line edit for "id" edit
+    */
     QLineEdit* idEdit;
+    /**
+    \brief
+    \param id : server id
+    */
     QString id;
 signals:
 
 public slots:
-
+    /**
+    \brief get the server id from idEdit
+    */
     void getIDSlot();
 
 };
