81b86fb2a274f0b27880be2b0ca0b7564312166b
zhang1984 <jzhang@studio-hamburg.de>
2014-07-30 14:56:40 +0200

    update cardelement documentation

--- GUI/cardelement.h
+++ GUI/cardelement.h
@@ -2,13 +2,30 @@
 #define CARDELEMENT_H
 #include <config/messagetype.h>
 #include <GUI/channelelement.h>
-
+/**
+    \brief define the card element, include the channelElement and messageType of the card
+*/
 class cardElement
 {
 public:
+    /**
+    \brief initial the class
+    */
     cardElement();
+    /**
+    \brief
+    \param videoCard : the name of the videoCard
+    */
     QString videoCard;
+    /**
+    \brief
+    \param channelList : the list of channelElement under the video card
+    */
     QList<channelElement> channelList;
+    /**
+    \brief
+    \param vMessageList : the list of messageType under the video card
+    */
     QList<messageType> vMessageList;
 };
 
