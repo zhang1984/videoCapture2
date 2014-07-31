23b823394cdc94d00ecaa9f8895beb4dc2289271
zhang1984 <jzhang@studio-hamburg.de>
2014-07-31 12:46:15 +0200

    update the ipemement documentation

--- GUI/ipelement.h
+++ GUI/ipelement.h
@@ -3,12 +3,30 @@
 
 #include <config/messagetype.h>
 #include <GUI/cardelement.h>
+/**
+\brief define the ip elements (server elements)
+*/
 class ipElement
 {
 public:
+    /**
+    \brief initial
+    */
     ipElement();
+    /**
+    \brief
+    \param ipAdd : the ip address of the server
+    */
     QString ipAdd;
+    /**
+    \brief
+    \param cardList : the list of the card under the server
+    */
     QList< cardElement> cardList;
+    /**
+    \brief
+    \param iMessageList : the messageList of the server
+    */
     QList<messageType> iMessageList;
 };
 
